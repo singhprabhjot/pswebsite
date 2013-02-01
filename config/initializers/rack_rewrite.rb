947d4af52361fdcad143a2454a13a8cf9afe4d22
Prabhjot Singh <prabhjot27@gmail.com>
2013-02-01 09:40:09 -0800

    rack change to double equal

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,5 +1,5 @@
 Prabhjot::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
   r301 %r{.*}, 'http://www.singhprabhjot.com$&', :if => Proc.new {|rack_env|
-    rack_env['SERVER_NAME'] != 'www.singhprabhjot.com'
+    rack_env['SERVER_NAME'] == 'singhprabhjot.com'
   }
 end
