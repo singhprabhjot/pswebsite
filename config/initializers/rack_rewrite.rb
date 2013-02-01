9e93dbad079d868d7fa1d8711f6ce18a5fdd9547
Prabhjot Singh <prabhjot27@gmail.com>
2013-02-01 09:36:06 -0800

    rack rewrite

---
+++ config/initializers/rack_rewrite.rb
@@ -0,0 +1,5 @@
+Prabhjot::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+  r301 %r{.*}, 'http://www.singhprabhjot.com$&', :if => Proc.new {|rack_env|
+    rack_env['SERVER_NAME'] != 'www.singhprabhjot.com'
+  }
+end
